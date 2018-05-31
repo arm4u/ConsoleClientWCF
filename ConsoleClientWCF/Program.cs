9e4c245acf5ba953db5a8ec602a572d32a959469
Arman <arman4u@gmail.com>
2018-05-31 23:50:40 +0200

    Add project files.

---
+++ ConsoleClientWCF/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleClientWCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceReference1.MyServiceClient client = new ServiceReference1.MyServiceClient();
+            ServiceReference1.Res res = client.MyFunc(new ServiceReference1.Req { Id = 6 });
+
+            Console.WriteLine(res.ID + " - " + res.Name + " - " + res.Birthdate);
+
+            Console.Read();
+        }
+    }
+}
